01219662db432a7f91f79713086ccf944d03bca8
JurhJurh <74958711+JurhJurh@users.noreply.github.com>
2021-01-06 23:58:02 +0100

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,11 @@
+<?php
+require "DataBase.php";
+$db = new DataBase();
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])) {
+    if ($db->dbConnect()) {
+        if ($db->signUp("users", $_POST['email'], $_POST['username'], $_POST['password'])) {
+            echo "Sign Up Success";
+        } else echo "Sign up Failed";
+    } else echo "Error: Database connection";
+} else echo "All fields are required";
+?>
